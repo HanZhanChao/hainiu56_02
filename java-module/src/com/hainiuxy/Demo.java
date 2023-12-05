52f881acce823110bb220ad13cd0435e57b81168
hainiu <hainiu@example.com>
2023-12-05 17:09:09 +0800

    java和mvn测试模块的2.0版本

--- java-module/src/com/hainiuxy/Demo.java
+++ java-module/src/com/hainiuxy/Demo.java
@@ -6,5 +6,8 @@ package com.hainiuxy;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(1);
+
+        System.out.println(2);
+        System.out.println(3);
     }
 }
